37666d4b762fa3b6133af07c02270be552fbb882
ChetanR0101 <chetan.rathod@prisms.in>
2022-06-07 11:39:00 +0530

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Pos",
+    "version":"1.0",
+    "author":"Prisms - PoS",
+    "description":"This App for PoS invoice",
+    "sequence":-100,
+    "depends":["base"],
+    "data":[
+            "security/ir.model.access.csv"
+
+            ],
+    "installable":True,
+    "application":True
+}
