4f1d341737b11ac1ddf6f35b41fe47d78ba27307
ChetanR0101 <chetan.rathod@prisms.in>
2022-06-08 11:20:13 +0530

    Checkout completed with compute function

--- models/Pos_checkout.py
+++ models/Pos_checkout.py
@@ -1,4 +1,4 @@
-from odoo import models,fields
+from odoo import api, models,fields
 
 
 # checkout
@@ -7,5 +7,22 @@ class PoS_checkout(models.Model):
 
     name= fields.Char(string="Customer Name")
     date=fields.Date("Date")
+
+    @api.depends('product_ids.amount')
+    @api.onchange('product_ids')
+    def cal_grand_sum(self):
+        self.grand_sum=0
+        for rec in self.product_ids:
+            self.grand_sum += rec.amount
+
+    grand_sum=fields.Float("Grand Sum",compute=cal_grand_sum,store=True)
+
+    invoice_id= fields.Char("Invoice Id")
+    @api.model
+    def create(self, vals_list):
+        res= super(PoS_checkout,self).create(vals_list)
+        res.invoice_id=f"{res.date}-{res.id}" 
+        return res
+
 
     product_ids= fields.One2many(comodel_name="pos.products_data",inverse_name="product_id")
--- models/Pos_products.py
+++ models/Pos_products.py
@@ -15,8 +15,8 @@ class PoS_products(models.Model):
     def create(self, vals_list):
         nxt_ran_int= random.randint(10,99)
         res= super(PoS_products,self).create(vals_list)
-        fst_let= res.name[0:3]
-        fst_let.upper()
+        fst_let= res.name[0:4]
+        fst_let=fst_let.upper()
         res.sku=fst_let+str(nxt_ran_int)+str(res.id)
         res.name=f"[{res.sku}] {res.name}"
         return res
@@ -24,8 +24,15 @@ class PoS_products(models.Model):
 class PoS_products_data(models.Model):
     _name='pos.products_data'
     name=fields.Many2one('pos.products',string="Product Name")
-    price= fields.Float("Price")
+    price= fields.Float("Price",related='name.price')
     qut= fields.Integer("Quntity")
+
+    @api.depends('price','qut')
+    def _cal_total(self):
+        for rec in self:
+            rec.amount= rec.price* rec.qut
+
+    amount=fields.Float("Total",compute=_cal_total,store=True)
 
     product_id= fields.Many2one(comodel_name="pos.checkout")
 
