c60ec3920a9bea6790ab2a86170b02d9732b01ae
ChetanR0101 <chetan.rathod@prisms.in>
2022-06-16 15:22:13 +0530

    previous + function name changed

--- static/src/js/select2_dropdown.js
+++ static/src/js/select2_dropdown.js
@@ -3,15 +3,8 @@ odoo.define("PoS.dropdown", function (require) {
     // alert("js running"); // used for debugging
 
     $(document).ready(function() {
-        // $("#solTitle a").click(function() {
-        //     //Do stuff when clicked
-        // });
-        
         console.log("Loaded... now"); // used for debugging
         setTimeout(MenuClick,10000);
-        // setTimeout(NewTestFun,7000);
-        // setTimeout(NewTestFun1,5000);
-
     });
 
 
@@ -20,24 +13,24 @@ odoo.define("PoS.dropdown", function (require) {
         console.log("Menu Clicked called"); // used for debugging
         $(".dropdown-item.o_nav_entry").click(function(){
             console.log("Menu... Clicked"); // used for debugging
-            setTimeout(NewTestFun,5000);
-            setTimeout(NewTestFun1,5000);
+            setTimeout(CheckoutClick,5000);
+            setTimeout(CheckinClick,5000);
     })};
-    function NewTestFun() {
+    function CheckoutClick() {
 
-        console.log("New Funtion called"); // used for debugging
+        console.log("CheckoutClick called"); // used for debugging
         $(".o_field_x2many_list_row_add a").click(function(){
             console.log("Clicked... Add"); // used for debugging
             setTimeout(checkContainer,3000);
     })};
 
-    function NewTestFun1() {
+    function CheckinClick() {
 
-        console.log("New Funtion1 called"); // used for debugging
+        console.log("CheckinClick called"); // used for debugging
         $(".btn.btn-primary.o_list_button_add").click(function(){
             console.log("Clicked... Create"); // used for debugging
             setTimeout(checkContainer,3000);
-            setTimeout(NewTestFun,3000);
+            setTimeout(CheckoutClick,3000);
     })};
         
 
